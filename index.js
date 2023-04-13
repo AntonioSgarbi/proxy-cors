0b3cf90ff4ea250a2e1b15d92c785443e7114db8
aokk <aokk@gft.com>
2023-04-13 14:20:57 -0300

    fix

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 const express = require('express');
 const cors = require('cors')
-const {response} = require("express");
-//cors for own server connected with own
 const app = express();
+const port = 5000;
 
-const port = process.env.PORT || 5000;
-
-//Middleware
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Server is ok')
+    res.send(`Running on port: ${port}`)
 });
 
 app.put('/', (req, res) => {
     const headers = {
-        'Content-Type': req.header('Content-Type'),
+        'Content-Type': 'application/json',
         'Authorization': req.headers.authorization
-
     }
+
     const requestOptions = {
         method: 'PUT',
         headers: headers,
-        body: req.body
+        body: JSON.stringify(req.body)
     };
 
-    fetch(req.query.url, requestOptions).then((response) => res.send(response))
+    fetch(req.query.url, requestOptions).then((response) => {
+        res.send(JSON.stringify(response));
+    })
 })
 
-
 app.listen(port, () => {
-    console.log('Port is Ok');
+    console.log(`Running on port: ${port}`);
 })
